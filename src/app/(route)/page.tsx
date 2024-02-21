7e721703d80e58ad48d28256f570a57be7ba5b18
HyerinJeon <hyerin0930@gmail.com>
2024-02-21 17:19:28 +0900

    :tada: Test api route

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((response) => response.json())
+      .then((data) => console.log("data", data));
+  }, []);
+
   return <div>Hello world!</div>;
 }
