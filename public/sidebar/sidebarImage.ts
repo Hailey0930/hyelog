547983010d653d89a383ce83b69a67f332c4abb4
HyerinJeon <hyerin0930@gmail.com>
2024-02-28 15:42:34 +0900

    :lipstick: Add sidebar images

---
+++ public/sidebar/sidebarImage.ts
@@ -0,0 +1,7 @@
+import hamburger from "./hamburger.png";
+import icon_email from "./icon_email.png";
+import icon_github from "./icon_github.png";
+import icon_linkedin from "./icon_linkedin.png";
+import profile from "./profile.jpg";
+
+export { hamburger, icon_email, icon_github, icon_linkedin, profile };
