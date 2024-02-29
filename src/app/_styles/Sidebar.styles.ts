b2ea797b7fc91b640e8cc39da320b04a4848ca7d
HyerinJeon <hyerin0930@gmail.com>
2024-02-29 15:53:30 +0900

    :lipstick: Edit Sidebar CSS in small breakpoints

--- src/app/_styles/Sidebar.styles.ts
+++ src/app/_styles/Sidebar.styles.ts
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import styled from "styled-components";
+import { breakPoints } from "./breakPoints";
 
 export const Container = styled.aside<{ $isSidebarOpen: boolean }>`
   display: flex;
@@ -13,6 +14,10 @@ export const Container = styled.aside<{ $isSidebarOpen: boolean }>`
   background-color: var(--secondary-color);
   transition: all 0.2s;
   overflow: hidden;
+
+  @media screen and (max-width: ${breakPoints.small}px) {
+    width: ${(props) => (props.$isSidebarOpen ? "100%" : "0px")};
+  }
 `;
 
 export const HamburgerContainer = styled.div`
