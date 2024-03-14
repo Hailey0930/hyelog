48f2c009646c68d5919dfb03a152b0d47fef91c6
HyerinJeon <hyerin0930@gmail.com>
2024-03-14 09:31:37 +0900

    :bug: Fix File bug'

--- src/app/_utils/fileUpload.ts
+++ src/app/_utils/fileUpload.ts
@@ -5,7 +5,7 @@ export const fileUpload = async (image: File | null) => {
 
   const blob = await put(image.name, image, {
     access: "public",
-    contentType: image.type,
+    contentType: "multipart/form-data",
   });
   return blob.url;
 };
