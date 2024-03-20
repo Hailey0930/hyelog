76070ac874786e3f9f1c33eafe4a5a2b1787c663
HyerinJeon <hyerin0930@gmail.com>
2024-03-20 15:11:51 +0900

    :art: Add deleteEmptyCategory in delete blog

--- src/app/api/blog/[blogId]/route.ts
+++ src/app/api/blog/[blogId]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../_lib/prisma";
 import { IBlog, IBlogParams } from "@/app/types/Blog.types";
+import { deleteEmptyCategory } from "@/app/_utils/deleteEmptyCategory";
 
 export async function GET(
   request: NextRequest,
@@ -23,9 +24,18 @@ export async function DELETE(request: NextRequest, { params }: IBlogParams) {
   try {
     const blogId = params.blogId || "";
 
+    // 블로그 정보 조회하여 카테고리 ID 얻기
+    const blog = await prisma.blog.findUnique({
+      where: { id: blogId },
+    });
+
+    const categoryId = blog?.categoryId;
+
     await prisma.blog.delete({
       where: { id: blogId },
     });
+
+    if (categoryId) await deleteEmptyCategory(categoryId);
 
     return NextResponse.json({ message: "삭제 성공", status: 200 });
   } catch (error) {
