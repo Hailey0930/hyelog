00775005abd1a6ba65bd43b75b4b00d100ff3282
HyerinJeon <hyerin0930@gmail.com>
2024-03-20 14:20:53 +0900

    :bug: Add force-dynamic for fix blogListAPI bug

--- src/app/api/blogList/route.ts
+++ src/app/api/blogList/route.ts
@@ -2,6 +2,7 @@ import prisma from "../../../_lib/prisma";
 import { IBlog } from "@/app/types/Blog.types";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
 export async function GET(): Promise<NextResponse<IBlog[]> | undefined> {
   try {
     const blogs = await prisma.blog.findMany({
